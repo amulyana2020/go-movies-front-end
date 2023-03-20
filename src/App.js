b4c40bbf7da7cdd1cd90eeb37a280db0bac18dc3
Ai Mulyana <ai.mulyana@outlook.com>
2023-03-20 10:34:49 +0700

    Adding React Router Version 6

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         </div>
         <div className='col text-end'>
           <a
-            href='#'
+            href='#!'
             className='btn btn-success mt-4'
           >
             <span>Login</span>
@@ -25,37 +25,37 @@ function App() {
           <nav>
             <div className='list-group'>
               <a
-                href='#'
+                href='#!'
                 className='list-group-item list-group-item-action'
               >
                 Home
               </a>
               <a
-                href='#'
+                href='#!'
                 className='list-group-item list-group-item-action'
               >
                 Movies
               </a>
               <a
-                href='#'
+                href='#!'
                 className='list-group-item list-group-item-action'
               >
                 Genres
               </a>
               <a
-                href='#'
+                href='#!'
                 className='list-group-item list-group-item-action'
               >
                 Add Movie
               </a>
               <a
-                href='#'
+                href='#!'
                 className='list-group-item list-group-item-action'
               >
                 Manage Catalog
               </a>
               <a
-                href='#'
+                href='#!'
                 className='list-group-item list-group-item-action'
               >
                 Graphql
